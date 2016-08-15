a3a350ed69a96cc80b209a24b3e3251b2a0d9879
David Jeria <djeria@alum.mit.edu>
2016-08-14 22:51:14 -0400

    Run main via pool.map

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import sys
 import time
 import logging
+from multiprocessing import Pool
 from dataCollector import DataCollector
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+def collect_betfair_data(market_id):
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-collector = DataCollector('1.126180340')
+    collector = DataCollector(market_id)
 
-while True:
-    try:
-        collector.run()
-        time.sleep(5)
-    except RuntimeError, e:
-        logging.exception("Runtime error")
-        sys.exit()
-    except Exception, e:
-        logging.exception("Unexpected error")
+    while True:
+        try:
+            collector.run()
+            time.sleep(5)
+        except RuntimeError, e:
+            logging.exception("Runtime error")
+            sys.exit()
+        except Exception, e:
+            logging.exception("Unexpected error")
+
+if __name__ == "__main__":
+    markets = [
+        '1.125246985',
+        '1.125800676',
+    ]
+
+    pool = Pool(4)
+    pool.map(collect_betfair_data, markets)
